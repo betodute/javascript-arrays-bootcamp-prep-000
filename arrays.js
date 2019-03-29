fb0eab3e80c7d4ea9906dbf164ae96c500c11b8d
Alberto Duarte <betodute@gmail.com>
2019-03-29 01:21:51 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -23,7 +23,7 @@ function accessElementInArray (array,index) {
   return array[index];
 }
 
-function destructivelyRemoveElementFromBeginningOfArray(chocolateBars){
+function destructivelyRemoveElementFromBeginningOfArray(chocolateBars.shift){
   return chocolateBars.shift() ;
 }
 
