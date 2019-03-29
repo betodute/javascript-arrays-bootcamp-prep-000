36c622449c298a8541f56c574f367b7c57e163a2
Alberto Duarte <betodute@gmail.com>
2019-03-29 01:20:36 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,6 @@
 //OMG Arrays Mode
 
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
-var candyString = "foo";
 
 function addElementToBeginningOfArray (chocolateBars,candyString) {
   return [candyString,...chocolateBars];
