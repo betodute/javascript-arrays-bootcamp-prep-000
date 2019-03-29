efea32b1ddb2365e1f20315428454b2176762b8a
Alberto Duarte <betodute@gmail.com>
2019-03-29 00:35:15 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -6,7 +6,6 @@ function addElementToBeginningOfArray (chocolateBars,candyString) {
   return [candyString,...chocolateBars];
 }
 
-}
 function accessElementInArray (array, index) {
   return array[index];
 }
