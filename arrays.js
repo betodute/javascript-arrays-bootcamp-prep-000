67b32dba8a0ea20416b835bcb8b85f013a3ae96b
Alberto Duarte <betodute@gmail.com>
2019-03-29 00:31:16 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,9 @@
 
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementToBeginningOfArray (chocolateBars,candyString
+function addElementToBeginningOfArray (chocolateBars,candyString){
+  return 
+}
 };
 
 }
