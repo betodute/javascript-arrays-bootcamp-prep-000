a9f26a28a32aa46bdc3a9b7dc75d6f42e6616350
Alberto Duarte <betodute@gmail.com>
2019-03-29 00:45:48 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -6,10 +6,6 @@ function addElementToBeginningOfArray (chocolateBars,candyString) {
   return [candyString,...chocolateBars];
 }
 
-function accessElementInArray (array, index) {
-  return array[index];
-}
-
 function destructivelyAddElementToBeginningOfArray (chocolateBars, candyString) {
   chocolateBars.unshift(candyString)
   return chocolateBars;
